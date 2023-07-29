009fdb461c834101a005455925a77940bd6fd365
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-07-29 01:14:45 -0400

    splitter added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+import logging
+import time
+import threading
+import itertools
+import sys
 from pytube import YouTube
 from datetime import datetime
 from moviepy.editor import *
 import whisper
 import os
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 def download_video_with_timestamp(url, output_path):
     try:
@@ -20,7 +28,7 @@ def download_video_with_timestamp(url, output_path):
         # Download the video using pytube
         video.download(output_path, filename=output_filename)
 
-        print("Video downloaded successfully!")
+        logging.info("Video downloaded successfully!")
 
         # Convert the downloaded video to mp3
         audio_file_path = f"{output_path}/{yt.title}_{timestamp}.mp3"
@@ -30,56 +38,34 @@ def download_video_with_timestamp(url, output_path):
         audio_clip.close()
         video_clip.close()
 
-        print("Video converted to mp3 successfully!")
+        logging.info("Video converted to mp3 successfully!")
         return audio_file_path
     except Exception as e:
-        print("Error:", str(e))
+        logging.error(f"Error: {str(e)}")
         return None
 
+def spinner():
+    write, flush = sys.stdout.write, sys.stdout.flush
+    for char in itertools.cycle('|/-\\'):
+        status = char + ' working...'
+        write(status)
+        flush()
+        time.sleep(0.1)
+        write('\x08' * len(status))
+
 def transcribe_audio(audio_file_path, model_path):
-
-    import whisper
-
-    # Load the Whisper model
-    # model = torch.load("./large-v2.pt")
-
+    # Load the whisper model
     model = whisper.load_model(model_path)
 
     # Transcribe audio
     result = model.transcribe(audio_file_path)
 
-    # Get the complete text transcription
-    # transcription_text = result["text"]
-
-    # Print the transcription
-    # print(transcription_text)
-    # Load the whisper model
-    
-
-    # # Load audio and pad/trim it to fit 30 seconds
-    # audio = whisper.load_audio(audio_file_path)
-    # audio = whisper.pad_or_trim(audio)
-
-    # # Make log-Mel spectrogram and move to the same device as the model
-    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    # # Detect the spoken language
-    # _, probs = model.detect_language(mel)
-    # print(f"Detected language: {max(probs, key=probs.get)}")
-
-    # # Decode the audio
-    # options = whisper.DecodingOptions(fp16=False)
-    # result = whisper.decode(model, mel, options)
-
-    # # Print the recognized text
-    # print(result.text)
-
     # Save the recognized text to a text file
     text_file_path = os.path.splitext(audio_file_path)[0] + ".txt"
     with open(text_file_path, "w") as text_file:
-        text_file.write(result['text'])
+        text_file.write(result.text)
 
-    print(f"Transcript saved to: {text_file_path}")
+    logging.info(f"Transcript saved to: {text_file_path}")
 
 # Usage
 video_url = "https://www.youtube.com/watch?v=YOolU9utNH0"
@@ -87,4 +73,14 @@ output_directory = "./videos"
 audio_file_path = download_video_with_timestamp(video_url, output_directory)
 
 if audio_file_path is not None:
-    transcribe_audio(audio_file_path, "large")
+    # Start the spinner in a separate thread
+    spin_thread = threading.Thread(target=spinner)
+    spin_thread.start()
+
+    # Perform the transcription
+    try:
+        transcribe_audio(audio_file_path, "large")
+    finally:
+        # Stop the spinner
+        spin_thread.do_run = False
+        spin_thread.join()
