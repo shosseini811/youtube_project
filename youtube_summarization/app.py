ba82c0764812dcbb204365b6b9af29a67fe62a4b
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-08-04 21:47:13 -0400

    code updated

--- youtube_summarization/app.py
+++ youtube_summarization/app.py
@@ -5,3 +5,5 @@ video_id = "H3d2yIHk8b4" # youtube video id
 transcript = YouTubeTranscriptApi.get_transcript(video_id) # get the transcript
 transcript_text = " ".join([entry['text'] for entry in transcript]) # get the transcript text
 transcript_text
+summary = summarizer(transcript_text, max_length=100, min_length=5, do_sample=False)[0]['summary_text']
+print(summary)
