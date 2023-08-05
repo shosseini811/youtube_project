aa5f7b9d917722f01685c529fc076bbb748f512a
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-08-04 21:35:20 -0400

    youtube summarization added

---
+++ youtube_summarization/app.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn") # 
+from youtube_transcript_api import YouTubeTranscriptApi 
+video_id = "H3d2yIHk8b4" # youtube video id
+transcript = YouTubeTranscriptApi.get_transcript(video_id) # get the transcript
+transcript_text = " ".join([entry['text'] for entry in transcript]) # get the transcript text
+transcript_text
